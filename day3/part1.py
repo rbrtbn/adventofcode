700fc2efd6890ac49cc48c1877829bb02848405a
Robert Ban <robert.ban@prezi.com>
2017-12-04 14:37:51 +0100

    Day 3 Part 2

--- day3/part1.py
+++ day3/part1.py
@@ -5,7 +5,7 @@ def get_ring_length(ring_number):
     return 4 * (ring_number * 2 - 1) + 4
 
 
-def distance_from_ring_middle(spiral_length, rings, position):
+def get_distance_from_ring_center(spiral_length, rings, position):
     if rings == 0:
         return 0
 
@@ -24,7 +24,7 @@ def get_distance_from_access_port(position):
         rings += 1
         spiral_length += get_ring_length(rings)
 
-    return distance_from_ring_middle(spiral_length, rings, position) + rings
+    return get_distance_from_ring_center(spiral_length, rings, position) + rings
 
 
 print(get_distance_from_access_port(289326))
