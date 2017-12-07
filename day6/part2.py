7cd7e217ba2d5ae4e66d8dbe9ede0abbca615e6b
Robert Ban <robert.ban@prezi.com>
2017-12-07 20:46:21 +0100

    Day 6

---
+++ day6/part2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+def get_state(memory_banks):
+    return ",".join(map(str, memory_banks))
+
+
+previous_states = []
+with open('input.txt') as file:
+    memory_banks = list(map(int, file.readline().strip().split("\t")))
+    cycles = 0
+    while get_state(memory_banks) not in previous_states:
+        previous_states.append(get_state(memory_banks))
+        most_blocks = max(memory_banks)
+        most_blocks_pos = memory_banks.index(most_blocks)
+        memory_banks[most_blocks_pos] = 0
+        for pos in range(most_blocks_pos + 1, most_blocks_pos + most_blocks + 1):
+            memory_banks[pos % len(memory_banks)] += 1
+        cycles += 1
+
+print(cycles - previous_states.index(get_state(memory_banks)))
