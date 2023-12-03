412751d5f0218ec34a711ab94eed157bd41c1982
Robert Ban <banrobert@gmail.com>
2023-12-03 13:57:47 +0100

    2023/01/1

---
+++ 2023/day01/part1.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	calibration := 0
+	scanner := bufio.NewScanner(file)
+	reDigit := regexp.MustCompile(`\d`)
+	for scanner.Scan() {
+		txt := scanner.Text()
+		digits := reDigit.FindAllString(txt, -1)
+
+		if len(digits) >= 1 {
+			firstDigit, _ := strconv.Atoi(digits[0])
+			lastDigit, _ := strconv.Atoi(digits[len(digits)-1])
+
+			calibration += 10*firstDigit + lastDigit
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(calibration)
+}
