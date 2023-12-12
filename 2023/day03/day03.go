7a958873d5e64b17f70f71aea97128cea0e0e7ba
Robert Ban <banrobert@gmail.com>
2023-12-12 07:44:22 +0100

    day 3 / part 2

--- 2023/day03/day03.go
+++ 2023/day03/day03.go
@@ -1,7 +1,6 @@
 package day03
 
 import (
-	"fmt"
 	"regexp"
 	"strconv"
 	"strings"
@@ -10,20 +9,25 @@ import (
 type part struct {
 	isSymbol bool
 	isNumber bool
-	number   int
-	position int
+	number   *int
 }
 
 func Part1(input string) int {
 	engine := parseInput(input)
 
 	sum := 0
+	partNumbers := []*int{}
 	for _, parts := range engine {
 		for _, part := range parts {
-			if part.isNumber && part.position == 0 {
-				sum += part.number
+			if part.isNumber {
+				partNumbers = append(partNumbers, part.number)
 			}
 		}
+	}
+
+	partNumbers = removeDuplicates(partNumbers)
+	for _, partNumber := range partNumbers {
+		sum += *partNumber
 	}
 
 	return sum
@@ -36,22 +40,13 @@ func Part2(input string) int {
 	for i, parts := range engine {
 		for j, part := range parts {
 			if part.isSymbol {
-				sum += i + j
-				// neighbours := getNeighbours(parts, i, j)
-				// if len(neighbours) > 0 {
-				// 	min := neighbours[0][0]
-				// 	for _, neighbour := range neighbours {
-				// 		if neighbour[0] < min {
-				// 			min = neighbour[0]
-				// 		}
-				// 	}
-				// 	parts[i][j][0] = min
-				// }
+				neighbours := getNeighbours(engine, i, j)
+				if len(neighbours) == 2 {
+					sum += *neighbours[0] * *neighbours[1]
+				}
 			}
 		}
 	}
-
-	fmt.Println(engine)
 
 	return sum
 }
@@ -60,12 +55,14 @@ func parseInput(input string) [][]part {
 	lines := strings.Split(input, "\n")
 	lines = lines[:len(lines)-1]
 
+	notANumber := 0
+
 	engine := make([][]part, len(lines))
 	for i, line := range lines {
 		engine[i] = make([]part, len(line))
 		for j, char := range line {
 			if isSymbol(char) {
-				engine[i][j] = part{true, false, 0, 0}
+				engine[i][j] = part{true, false, &notANumber}
 			}
 		}
 	}
@@ -94,7 +91,7 @@ func storePartNumber(engine [][]part, i, j int, numStr string) [][]part {
 		partNumber, _ := strconv.Atoi(numStr)
 
 		for k := j - 1; k > j-len(numStr)-1; k-- {
-			engine[i][k] = part{false, true, partNumber, k - j + len(numStr)}
+			engine[i][k] = part{false, true, &partNumber}
 		}
 	}
 
@@ -113,6 +110,31 @@ func isPartNumber(engine [][]part, i, jMin, jMax int) bool {
 	}
 
 	return false
+}
+
+func getNeighbours(engine [][]part, i, j int) []*int {
+	partNumbers := []*int{}
+	for ii := i - 1; ii <= i+1; ii++ {
+		for jj := j - 1; jj <= j+1; jj++ {
+			if ii >= 0 && ii <= len(engine)-1 && jj >= 0 && jj <= len(engine[0])-1 && engine[ii][jj].isNumber {
+				partNumbers = append(partNumbers, engine[ii][jj].number)
+			}
+		}
+	}
+
+	return removeDuplicates(partNumbers)
+}
+
+func removeDuplicates(sliceList []*int) []*int {
+	allKeys := make(map[*int]bool)
+	list := []*int{}
+	for _, item := range sliceList {
+		if _, value := allKeys[item]; !value {
+			allKeys[item] = true
+			list = append(list, item)
+		}
+	}
+	return list
 }
 
 func isNumber(symbol rune) bool {
