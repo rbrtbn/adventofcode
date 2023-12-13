b5bd11f4ecd41161d28e5305969e8e2663c95150
Robert Ban <banrobert@gmail.com>
2023-12-13 21:15:53 +0100

    download input files

---
+++ 2023/day04/day04.go
@@ -0,0 +1,5 @@
+package day04
+
+func Part1() int {
+	return 0
+}
--- 2023/main.go
+++ 2023/main.go
@@ -1,22 +1,47 @@
 package main
 
 import (
+	"aoc2023/day04"
 	"bufio"
 	"day01"
 	"day02"
 	"day03"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"strconv"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	action, day := parseArgs(os.Args[1:])
 
-	args := os.Args[1:]
+	if action == "download" {
+		fetchInput(day)
+	} else if action == "solve" {
+		solve(day)
+	}
+}
 
-	day, _ := strconv.Atoi(args[0])
+func parseArgs(args []string) (string, int) {
+	var action string
+	var day int
 
+	if len(args) == 1 {
+		action = "solve"
+		day, _ = strconv.Atoi(args[0])
+	} else {
+		action = args[0]
+		day, _ = strconv.Atoi(args[1])
+	}
+
+	return action, day
+}
+
+func solve(day int) {
 	switch day {
 	case 1:
 		day01.Part1()
@@ -30,12 +55,14 @@ func main() {
 		res2 := day03.Part2(input)
 		fmt.Println(res1, res1 == sol1)
 		fmt.Println(res2, res2 == sol2)
+	case 4:
+		res1 := day04.Part1()
+		fmt.Println(res1)
 	}
 }
 
 func readInputFile(day int) string {
-	folder := fmt.Sprintf("day%02d", day)
-	file, err := os.Open(folder + "/input.txt")
+	file, err := os.Open(dayPath(day) + "/input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -55,8 +82,7 @@ func readInputFile(day int) string {
 }
 
 func readSolutionFile(day int) (int, int) {
-	folder := fmt.Sprintf("day%02d", day)
-	file, err := os.Open(folder + "/solution.txt")
+	file, err := os.Open(dayPath(day) + "/solution.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -75,4 +101,42 @@ func readSolutionFile(day int) (int, int) {
 	}
 
 	return sol1, sol2
+}
+
+func fetchInput(day int) {
+	godotenv.Load()
+	url := fmt.Sprintf("https://adventofcode.com/2023/day/%d/input", day)
+	sessionCookie := os.Getenv("SESSION_COOKIE")
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("Cookie", "session="+sessionCookie)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		log.Fatal("Unexpected status code in response:", resp.StatusCode)
+	}
+
+	file, err := os.Create(dayPath(day) + "/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func dayPath(day int) string {
+	return fmt.Sprintf("day%02d", day)
 }
